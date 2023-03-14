f6021007cc879adbb5e6032ff78d608179259340
sdorado2 <sdorado2@gmail.com>
2023-03-14 18:00:18 -0400

    search component readjusted;drinks needs more adjustments

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
+// import { Link, useParams } from "react-router-dom";
 import Search from "../components/Search";
 
 const Drinks = (props) => {
-  const [drink, setDrink] = useState({
-    searchTerm: "",
-  });
+  const [drink, setDrink] = useState(null);
 
-  const url = `www.thecocktaildb.com/api/json/v1/1/search.php?s={""}`;
+  // const url = `www.thecocktaildb.com/api/json/v1/1/search.php?s={""}`;
 
   const getDrink = async (searchTerm) => {
-    const response = await fetch({ url });
+    const response = await fetch(
+      `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`
+    );
     const data = await response.json();
+    console.log("data has :", data);
     setDrink(data);
   };
 
@@ -20,9 +21,18 @@ const Drinks = (props) => {
     getDrink("Margarita");
   }, []);
 
+  console.log("drink has : ", drink);
+  // console.log("drink has : ", drink[0]);
+
   return (
     <div>
-      <Search />
+      <div>
+        <Search drinksearch={getDrink} />
+      </div>
+
+      <div>
+        <h1>{drink}</h1>
+      </div>
     </div>
   );
 };
